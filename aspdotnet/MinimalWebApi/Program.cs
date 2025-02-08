fa69e64ce65f409eb0ce40e2d203482721dfa579
Kuroki-g <64629990+Kuroki-g@users.noreply.github.com>
2025-02-08 18:00:49 +0900

    add OpenAPI documentation support for TodoAPI

--- aspdotnet/MinimalWebApi/Program.cs
+++ aspdotnet/MinimalWebApi/Program.cs
@@ -6,6 +6,14 @@ using MinimalWebApi.Models;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<TodoDb>(opt => opt.UseInMemoryDatabase("TodoList"));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddOpenApiDocument(config =>
+{
+    config.DocumentName = "TodoAPI";
+    config.Title = "TodoAPI v1";
+    config.Version = "v1";
+});
 var app = builder.Build();
 
 app.MapGet("/todoitems", async (TodoDb db) =>
@@ -55,3 +63,5 @@ app.MapDelete("/todoitems/{id}", async (int id, TodoDb db) =>
 });
 
 app.Run();
+
+public partial class Program { }
